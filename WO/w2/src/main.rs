e5118d7be55ba2a9fc190a3f05cd1dff0e023e9d
liqowomo <123171512+liqowomo@users.noreply.github.com>
2025-03-19 17:12:52 +0000

    🫦

--- WO/w2/src/main.rs
+++ WO/w2/src/main.rs
@@ -1,9 +1,8 @@
 // Main Entry Point will be here
-
+#[warn(dead_code)]
 mod test1; // This is to import the enire module
-use test1::{ban1, col_text1}; // This is calling a specific function
+use test1::ban1; // This is calling a specific function
 
 fn main() {
     ban1();
-    col_text1();
 }
