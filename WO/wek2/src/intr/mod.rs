121fef889cc373c86dc75961609d5e5bfe3772fe
liqowomo <123171512+liqowomo@users.noreply.github.com>
2025-03-20 20:31:35 +0000

    🫦

--- WO/wek2/src/intr/mod.rs
+++ WO/wek2/src/intr/mod.rs
@@ -1,12 +1,20 @@
 // Introduction to Rust Chapter
 
 use crate::utils::print_with_fire_gradient;
+use yansi::Paint;
 
 pub fn intr_main() {
     bannr();
+    demo_variable_assignment();
 }
 
 fn bannr() {
     let string = "Introduction to Rust".to_string();
     print_with_fire_gradient(string);
+}
+
+fn demo_variable_assignment() {
+    let message = "Panty Smellers";
+    let fetish_numer = 69;
+    let booty_weight = 190;
 }
