530091dbc2715d3154df0a9e0d77b52558d790d5
liqowomo <123171512+liqowomo@users.noreply.github.com>
2025-03-20 20:53:37 +0000

    🫦

--- WO/wek2/src/utils.rs
+++ WO/wek2/src/utils.rs
@@ -11,7 +11,7 @@ pub fn print_with_fire_gradient(text: String) {
 
     say(Options {
         text,
-        font: Fonts::FontChrome,
+        font: Fonts::FontPallet,
         gradient: fire_gradient,
         independent_gradient: false,
         transition_gradient: true,
