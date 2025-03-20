fdb892e77a135d95377d75fd2a22a44346e22746
liqowomo <123171512+liqowomo@users.noreply.github.com>
2025-03-20 20:57:00 +0000

    🫦

--- WO/wek2/src/utils.rs
+++ WO/wek2/src/utils.rs
@@ -4,15 +4,18 @@ use cfonts::{Fonts, Options, say};
 
 pub fn print_with_fire_gradient(text: String) {
     // Define the fire gradient colors
-    let fire_gradient = vec![
-        String::from("#FF4500"), // Firebrick red
-        String::from("#FFFF00"), // Yellow
+    let synth = vec![
+        String::from("#FF00FF"), // Neon Pink
+        String::from("#8A2BE2"), // Blue Violet
+        String::from("#00FFFF"), // Cyan
+        String::from("#FF1493"), // Deep Pink
+        String::from("#9400D3"), // Dark Violet
     ];
 
     say(Options {
         text,
         font: Fonts::FontTiny,
-        gradient: fire_gradient,
+        gradient: synth,
         independent_gradient: false,
         transition_gradient: true,
         ..Options::default()
