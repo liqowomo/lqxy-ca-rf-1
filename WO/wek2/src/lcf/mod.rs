44f30b62c57458871cccb764a5c236248bea3834
liqowomo <123171512+liqowomo@users.noreply.github.com>
2025-03-21 17:54:45 +0000

    🫦

--- WO/wek2/src/lcf/mod.rs
+++ WO/wek2/src/lcf/mod.rs
@@ -3,7 +3,6 @@
 #![allow(unused_imports)]
 
 use crate::utils::{header, print_with_synthwave_gradient};
-use std::io;
 use yansi::Paint;
 
 // Main function that will call other functions in the file
@@ -16,18 +15,11 @@ pub fn lcf_main() {
 
 fn loops_1() {
     header("Loop Test");
-    // let mut x = 1;
-    println!("Enter Number Of Pussy to Smell: ");
-    let x: i32 = io::stdin()
-        .read_line(&mut String::new())
-        .expect("Failed to read line")
-        .trim()
-        .parse()
-        .expect("Please type a valid number");
-
+    let mut x = 1;
     // continue loop until x > 5
     loop {
         println!("{}, {}", "x Looped to".blue(), x.magenta());
+        x += 1;
         if x > 5 {
             break;
         }
