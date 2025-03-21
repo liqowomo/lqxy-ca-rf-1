cffafb0faf01845ea37e4310f28a09c052989747
liqowomo <123171512+liqowomo@users.noreply.github.com>
2025-03-21 21:31:44 +0000

    🫦

--- WO/wek2/src/lcf/mod.rs
+++ WO/wek2/src/lcf/mod.rs
@@ -84,10 +84,10 @@ fn while_loop_advanced_1() {
     let mut input = String::new();
 
     while input.trim() != "fuck" {
-        input.clear();
         println!("Enter Work (type fuck to exit):");
         io::stdin().read_line(&mut input).expect("😡 Failed");
         println!("You Entered : {}", input.magenta().blink());
+        input.clear();
     }
 
     println!("👃Smelly Woman Pussy")
