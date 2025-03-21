e8df172b26a127d818a97a178ddca6e7d067d264
liqowomo <123171512+liqowomo@users.noreply.github.com>
2025-03-21 21:28:21 +0000

    🫦

--- WO/wek2/src/lcf/mod.rs
+++ WO/wek2/src/lcf/mod.rs
@@ -84,8 +84,8 @@ fn while_loop_advanced_1() {
     let mut input = String::new();
 
     while input.trim() != "fuck" {
-        println!("Enter Work (type fuck to exit):");
         input.clear();
+        println!("Enter Work (type fuck to exit):");
         io::stdin().read_line(&mut input).expect("😡 Failed");
         println!("You Entered : {}", input.magenta().blink());
     }
