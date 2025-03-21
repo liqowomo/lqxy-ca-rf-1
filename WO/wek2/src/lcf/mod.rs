5056a35ce1bc7518447069dc8c712654d6c79ad3
liqowomo <123171512+liqowomo@users.noreply.github.com>
2025-03-21 17:59:50 +0000

    🫦

--- WO/wek2/src/lcf/mod.rs
+++ WO/wek2/src/lcf/mod.rs
@@ -3,12 +3,13 @@
 #![allow(unused_imports)]
 
 use crate::utils::{header, print_with_synthwave_gradient};
+use std::io;
 use yansi::Paint;
 
 // Main function that will call other functions in the file
 pub fn lcf_main() {
     print_with_synthwave_gradient("Loops + Control Flow".to_string());
-    loops_1();
+    loops_2();
 }
 
 // Loops function
@@ -32,7 +33,7 @@ fn loops_2() {
     let mut x = 1;
     let mut input = String::new();
     println!("Enter a number to loop to: ");
-    std::io::stdin().read_line(&mut input).unwrap();
+    io::stdin().read_line(&mut input).unwrap();
     let input: i32 = input.trim().parse().unwrap();
     loop {
         println!("{}, {}", "x Looped to".blue(), x.magenta());
