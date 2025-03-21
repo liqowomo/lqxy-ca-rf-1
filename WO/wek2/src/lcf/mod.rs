8cb8ca0097ee2a7be8d476c9bf154cbc379d2d7e
liqowomo <123171512+liqowomo@users.noreply.github.com>
2025-03-21 17:46:17 +0000

    🫦

--- WO/wek2/src/lcf/mod.rs
+++ WO/wek2/src/lcf/mod.rs
@@ -8,15 +8,21 @@ use yansi::Paint;
 // Main function that will call other functions in the file
 pub fn lcf_main() {
     print_with_synthwave_gradient("Loops + Control Flow".to_string());
+    loops_1();
 }
 
 // Loops function
 
 fn loops_1() {
+    header("Loop Test");
     let mut x = 1;
 
     // continue loop until x > 5
     loop {
-        println!("{}, {}", "x is".blue(), x);
+        println!("{}, {}", "x is".blue(), x.magenta());
+        x += 1;
+        if x > 5 {
+            break;
+        }
     }
 }
