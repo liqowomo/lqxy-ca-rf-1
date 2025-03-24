54168be266031812b973e91c042f44e97434513c
liqowomo <123171512+liqowomo@users.noreply.github.com>
2025-03-24 16:28:48 +0000

    🫦

---
+++ WO/yrm/src/w1.rs
@@ -0,0 +1,9 @@
+// Work 1 from the tutorial
+
+use crate::utils::{clear_console, header, print_with_synthwave_gradient};
+
+// Main Function entry point
+
+pub fn w1_main() {
+    print_with_synthwave_gradient("Booty Smells")
+}
