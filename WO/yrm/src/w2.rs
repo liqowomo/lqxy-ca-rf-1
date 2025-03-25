920d8244702e3770f1861803200dd167071f4375
liqowomo <123171512+liqowomo@users.noreply.github.com>
2025-03-25 22:42:47 +0000

    🫦

--- WO/yrm/src/w2.rs
+++ WO/yrm/src/w2.rs
@@ -4,15 +4,20 @@
 #![allow(unused_variables)]
 
 // --- Imports Section ---
-
 use crate::utils::{clear_console, header, print_with_synthwave_gradient};
 use yansi::Paint;
 
 // --- Functions Section --
+
+// Main Function entry point
 
 pub fn w2_main() {
     // Banner Section
     let banner = "Ownership Model ";
     print_with_synthwave_gradient(banner.to_string());
     println!("{}", "Rust Ownership Model".green());
+
+    // Calling Sub Functions
 }
+
+// --- Sub functions to be called in main ---
