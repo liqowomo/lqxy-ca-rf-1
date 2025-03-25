95e6867113c4cd4a748559ff3db56843ab1f804c
liqowomo <123171512+liqowomo@users.noreply.github.com>
2025-03-25 22:41:06 +0000

    🫦

--- WO/yrm/src/w2.rs
+++ WO/yrm/src/w2.rs
@@ -2,3 +2,17 @@
 #![allow(dead_code)]
 #![allow(unused_imports)]
 #![allow(unused_variables)]
+
+// --- Imports Section ---
+
+use crate::utils::{clear_console, header, print_with_synthwave_gradient};
+use yansi::Paint;
+
+// --- Functions Section --
+
+pub fn w2_main() {
+    // Banner Section
+    let banner = "Ownership Model ";
+    print_with_synthwave_gradient(banner.to_string());
+    println!("{}", "Rust Ownership Model".green());
+}
