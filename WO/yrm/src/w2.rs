199801ef5e57b78e81a8c33f47a8d614fc4f6a07
liqowomo <123171512+liqowomo@users.noreply.github.com>
2025-03-26 21:57:42 +0000

    🫦

--- WO/yrm/src/w2.rs
+++ WO/yrm/src/w2.rs
@@ -36,6 +36,21 @@ impl MyPreciousRing {
     }
 }
 
+// This is declared as a module inside this file
+mod we_are_all_friends_here {
+    pub struct MyPreciousRing {
+        engraving: &'static str,
+    }
+
+    impl MyPreciousRing {
+        pub fn forge() -> Self {
+            Self {
+                engraving: "The One Ring Panty",
+            }
+        }
+    }
+}
+
 #[inline(never)] // This instruction forces the function to be seperately compiled
 fn ownership_ring() {
     let saurons_ring = MyPreciousRing::forge();
